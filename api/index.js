ee6045dfa9590ae62b826d7db8b99f3643dc6495
MliliGenes <saad.elm.77@gmail.com>
2024-03-15 02:09:44 +0000

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT;
+const url = process.env.MONGO;
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
   if (!err) {
     console.log("server connected!");
   }
+});
+
+mongoose.connect(url).then(() => {
+  console.log("connected to db");
 });
